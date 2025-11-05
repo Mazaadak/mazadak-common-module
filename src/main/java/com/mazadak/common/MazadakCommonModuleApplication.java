7b074d712871d8253c2bbb23fd4a285bd8d4aaab
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-11-05 18:16:59 +0200

    chore: project setup

---
+++ src/main/java/com/mazadak/common/MazadakCommonModuleApplication.java
@@ -0,0 +1,13 @@
+package com.mazadak.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazadakCommonModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MazadakCommonModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mazadak/common/MazadakCommonModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazadak.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazadakCommonModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
